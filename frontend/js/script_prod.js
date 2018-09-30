100603c339a100e10ba185b622a73caebf046405
Mehul Mohan <mehulmpt@gmail.com>
2018-09-30 20:23:29 +0530

    Phone fixes
    Detects and works on devices

--- frontend/js/script_prod.js
+++ frontend/js/script_prod.js
@@ -47,6 +47,34 @@ req2.onload = function () {
 req1.send();
 req2.send();
 
+// @if phone
+
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+}
+
+if(isMobile.any()) {
+	document.getElementById('forward').style.display = 'block'
+	document.getElementById('rewind').style.display = 'block'
+}
+
 // debouncer
 function debounce(func, wait, immediate) {
 	var timeout = void 0;
