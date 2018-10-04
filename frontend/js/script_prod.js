01a60781d53d2dc4359cbe2113461432ff0fd01e
Mehul Mohan <mehulmpt@gmail.com>
2018-10-05 03:28:42 +0530

    Final waves version

--- frontend/js/script_prod.js
+++ frontend/js/script_prod.js
@@ -1,200 +1,201 @@
-// @if phone
+'use strict';
 
-const isMobile = {
-	Android: function Android() {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function BlackBerry() {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function iOS() {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function Opera() {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function Windows() {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function any() {
-		return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
-	}
-}
+// @if phone
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (f) {
+	return setTimeout(f, 1000 / 60);
+}; // simulate calling code 60
 
 // constants
 //const REWIND = 'REWIND'
-const FORWARD = 'FORWARD'
-const minCap = 1000
+var FORWARD = 'FORWARD';
+var minCap = 1000;
 
-const container = document.getElementById('video')
-const playerForward = document.getElementById('playerForward')
+var container = document.getElementById('video');
+var playerForward = document.getElementById('playerForward');
 
 // preloading
-var req1 = new XMLHttpRequest()
-req1.open('GET', '/video/dark.mp4', true)
-req1.responseType = 'blob'
+var req1 = new XMLHttpRequest();
+req1.open('GET', '/video/dark.mp4', true);
+req1.responseType = 'blob';
 
-req1.onload = function() {
-	// Onload is triggered even on 404
-	// so we need to check the status code
+req1.onload = function () {
 	if (this.status === 200) {
-	   var videoBlob = this.response
-	   var vid = URL.createObjectURL(videoBlob) // IE10+
-	   // Video is now downloaded
-	   // and we can set it as source on the video element
-	   playerForward.src = vid
-	}
-}
+		var videoBlob = this.response;
+		var vid = URL.createObjectURL(videoBlob); // IE10+
+		playerForward.src = vid;
 
-req1.send()
+		var elem = document.getElementById('pre-load'); //.style.display = 'none';
+		elem.parentNode.removeChild(elem);
+	}
+};
+
+req1.send();
 
 // debouncer
 function debounce(func, wait, immediate) {
-	let timeout
-	return function(...args) {
-		const later = () => {
-			timeout = null
-			if (!immediate) func(...args)
+	var timeout = void 0;
+	return function () {
+		for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+			args[_key] = arguments[_key];
 		}
-		
-		const callNow = immediate && !timeout
 
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		if (callNow) func(...args)
-	}
+		var later = function later() {
+			timeout = null;
+			if (!immediate) func.apply(undefined, args);
+		};
+
+		var callNow = immediate && !timeout;
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(undefined, args);
+	};
 }
 
 // resetters
-let seekTimer
-let intervalRewind
+var seekTimer = void 0;
+var intervalRewind = void 0;
 
-let activePlayer = playerForward
-let mainSEEK = FORWARD
+var activePlayer = playerForward;
+var mainSEEK = FORWARD;
 
-playerForward.onloadeddata = function() {
-	playerForward.playbackRate = 0
-	playerForward.play()
-}
+playerForward.onloadeddata = function () {
+	playerForward.playbackRate = 0;
+	playerForward.play();
+};
 
-playerForward.onended = function() {
-	playerForward.playbackRate = 0
-	playerForward.play()
-}
+playerForward.onended = function () {
+	playerForward.playbackRate = 0;
+	playerForward.pause();
 
-const videoInfo = {
-	_speed: 0,
-	get speed() {
-		return this._speed
-	},
-	set speed(val) {
-		if(val < 0.0625 && val !== 0) val = 0.0625 // because val = 0 is allowed
-		if(val > 2.5) val = 2.5 // cap
-		this._speed = val
-		activePlayer.playbackRate = this._speed
+	var register = document.getElementById('registerEnd');
+
+	register.style.display = 'flex';
+	register.style.opacity = 0;
+
+	requestAnimationFrame(showRegister);
+};
+
+function showRegister() {
+
+	var register = document.getElementById('registerEnd');
+
+	if (+register.style.opacity < 1) {
+		register.style.opacity = +register.style.opacity + 0.1;
+		requestAnimationFrame(showRegister);
 	}
 }
 
-let ts, touchduration = 20, revTimer, forTimer
+var videoInfo = {
+	_speed: 0,
+	get speed() {
+		return this._speed;
+	},
+	set speed(val) {
+		if (val < 0.0625 && val !== 0) val = 0.0625; // because val = 0 is allowed
+		if (val > 2.5) val = 2.5; // cap
+		this._speed = val;
+		activePlayer.playbackRate = this._speed;
+	}
+};
+
+var ts = void 0,
+    touchduration = 20,
+    revTimer = void 0,
+    forTimer = void 0;
+
+function touchStart() {
+	console.log('Touching forward');
+
+	document.getElementById('forward').innerText = '';
+
+	clearInterval(forTimer);
+	forTimer = setInterval(function (_) {
+		var event = new CustomEvent('proceed');
+		event.forcedDelta = -3;
+		event.initEvent("proceed", false, true);
+		container.dispatchEvent(event);
+	}, touchduration);
+}
+
+function touchEnd() {
+	console.log('Touch forward over');
+
+	clearInterval(forTimer);
+}
 
 // Touch devices
 
-document.getElementById('forward').addEventListener('touchstart', e => {
-	console.log('Touching forward')
-	forTimer = setInterval(_ => {
-		var event = new CustomEvent('proceed')
-		event.forcedDelta = -3
-		event.initEvent("proceed", false, true)
-		container.dispatchEvent(event)
+document.getElementById('forward').addEventListener('touchstart', touchStart);
 
-	}, touchduration)
-})
-
-document.getElementById('forward').addEventListener('touchend', e => {
-	console.log('Touching forward over')
-
-	clearInterval(forTimer)
-})
-
+document.getElementById('forward').addEventListener('touchend', touchEnd);
 
 // Mouse
 
-document.addEventListener('mousedown', e => {
-	console.log('Mouse down')
+document.addEventListener('mousedown', touchStart, false);
 
-	forTimer = setInterval(_ => {
-		var event = new CustomEvent('proceed')
-		event.forcedDelta = -3
-		event.initEvent("proceed", false, true)
-		container.dispatchEvent(event)
+document.addEventListener('mouseup', touchEnd, false);
 
-	}, touchduration)
+container.addEventListener('proceed', debounce(handleScroll, touchduration / 2, true), false);
 
-}, false)
+function handleScroll(e) {
+	var delta, seekDirection;
+	return Promise.resolve().then(function () {
+		delta = e.forcedDelta; // forcedDelta => touchmove => mobile
 
-document.addEventListener('mouseup', e => {
-	
-	console.log('Mouse forward over')
+		seekDirection = delta > 0 ? REWIND : FORWARD;
+		return !activePlayer.paused && activePlayer.play();
+	}).then(function () {
 
-	clearInterval(forTimer)
-	
-}, false)
+		clearTimeout(seekTimer);
+		//console.log('Requesting and setting speed')
 
-container.addEventListener('proceed', debounce(handleScroll, touchduration/2, true), false)
 
-async function handleScroll(e) {
-	const delta = e.forcedDelta // forcedDelta => touchmove => mobile
-
-	const seekDirection = delta > 0 ? REWIND : FORWARD
-
-	!activePlayer.paused && await activePlayer.play()
-
-	clearTimeout(seekTimer)
-	//console.log('Requesting and setting speed')
-	
-	
-	//if(delta < 0) {
+		//if(delta < 0) {
 		// scrolling forward, +ve playbackrate
-	if(isNaN(videoInfo.speed)) videoInfo.speed = 0
-	videoInfo.speed += Math.abs(delta)
-	//} else {
+		if (isNaN(videoInfo.speed)) {
+			videoInfo.speed = 0;
+		}videoInfo.speed += Math.abs(delta);
+		//} else {
 		// scrolling back but -ve playback not allowed
-	//	videoInfo.negSpeed += Math.abs(delta)
-	//}
-	//console.log('Speed set')
+		//	videoInfo.negSpeed += Math.abs(delta)
+		//}
+		//console.log('Speed set')
 
-	seekTimer = setTimeout(() => {
-		const intv = setInterval(() => {
-			//console.log('Requesting speed')
-			let deacceleration
-			const speed = videoInfo.speed
-			
-			if(speed > 2) { // too much speed, reduce fast
-				deacceleration = 0.1*speed
-			} else if(speed > 1.5) {
-				deacceleration = 0.07*speed
-			} else {
-				deacceleration = 0.05*speed
-			}
-			//videoInfo.speed -= 0.05*videoInfo.speed
-			//if(delta < 0) {
-			//	console.log('Forward deacc')
-				videoInfo.speed = speed - deacceleration
-		//	} else {
-		//		console.log('Rewind deacc')
-		//		rewind(speed - deacceleration)
-				videoInfo.negSpeed = speed - deacceleration
-		//	}
-		//	console.log('Requesting speed 2')
-			if(videoInfo.speed < 0.07) {
-		//		console.log('Setting speed 2')
-				videoInfo.speed = 0
-				clearInterval(intv)
-			} else {
-				//console.log(videoInfo.speed)
-			}
-		}, 10)
-	}, 50)
+		seekTimer = setTimeout(function () {
+			var intv = setInterval(function () {
+				//console.log('Requesting speed')
+				var deacceleration = void 0;
+				var speed = videoInfo.speed;
 
-	//console.log(e.wheelDeltaY)
+				if (speed > 2) {
+					// too much speed, reduce fast
+					deacceleration = 0.1 * speed;
+				} else if (speed > 1.5) {
+					deacceleration = 0.07 * speed;
+				} else {
+					deacceleration = 0.05 * speed;
+				}
+				//videoInfo.speed -= 0.05*videoInfo.speed
+				//if(delta < 0) {
+				//	console.log('Forward deacc')
+				videoInfo.speed = speed - deacceleration;
+				//	} else {
+				//		console.log('Rewind deacc')
+				//		rewind(speed - deacceleration)
+				videoInfo.negSpeed = speed - deacceleration;
+				//	}
+				//	console.log('Requesting speed 2')
+				if (videoInfo.speed < 0.07) {
+					//		console.log('Setting speed 2')
+					videoInfo.speed = 0;
+					clearInterval(intv);
+				} else {
+					//console.log(videoInfo.speed)
+				}
+			}, 10);
+		}, 50);
+
+		//console.log(e.wheelDeltaY)
+	});
 }
